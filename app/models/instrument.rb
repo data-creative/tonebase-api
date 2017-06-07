e3e63ebe9dbca30530c8586f2980060a97b420c6
MJ Rossetti <datacreativellc@gmail.com>
2017-06-07 13:55:08 -0400

    Enforce destruction of additional child associations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,6 @@
 class Instrument < ApplicationRecord
   validates :name, {presence: true, uniqueness: true}
 
-  has_many :ad_instruments
+  has_many :ad_instruments, dependent: :destroy
   has_many :ads, through: :ad_instruments
 end
--- spec/models/ad_spec.rb
+++ spec/models/ad_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Ad, type: :model do
   describe "associations" do
     it { should belong_to(:advertiser) }
     it { should have_many(:ad_placements) }
-    it { should have_many(:ad_instruments) }
+    it { should have_many(:ad_instruments).dependent(:destroy) }
     it { should have_many(:instruments).through(:ad_instruments) }
   end
 
