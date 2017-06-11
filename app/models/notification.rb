08ddcdee02912bf7e4b73992528e5f698053ed17
MJ Rossetti <datacreativellc@gmail.com>
2017-06-11 14:39:41 -0400

    Refactor

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,13 +4,11 @@ class Notification < ApplicationRecord
   has_many :user_notifications, dependent: :destroy
   has_many :users, through: :user_notifications
 
-  EVENT_TYPES = ["NewVideo", "NewAnnouncement"]
-
   validates_associated :broadcastable
   validates :broadcastable, {presence: true}
   validates :event, {
     presence: true,
-    inclusion: {in: EVENT_TYPES},
+    inclusion: {in: Broadcast::VALID_EVENTS},
     uniqueness: {scope: [:broadcastable_type, :broadcastable_id]}
   }
   validates :headline, {presence: true}
