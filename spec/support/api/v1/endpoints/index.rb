d9828be387bdaaa70fbef1bb5fecabd6ba0da904
MJ Rossetti <datacreativellc@gmail.com>
2017-06-16 18:53:37 -0400

    Restore the shared specs that somehow got lost

--- spec/support/api/v1/endpoints/index.rb
+++ spec/support/api/v1/endpoints/index.rb
@@ -1,65 +1,23 @@
 require_relative "../response"
 
 shared_examples_for "an index endpoint" do |model_class|
+  let!(:resource) { create(model_class.name.underscore.to_sym) }
+
   describe "response" do
-    before(:each) do
-      10.times do
-        create(model_class.name.underscore.to_sym)
-      end
-    end
+    context "when successful" do
+      let(:response){  get(:index, params: {format: 'json'})  }
 
-    context "with invalid pagination params (missing 'per_page')" do
-      let(:response){  get(:index, params: {page: 1, format: 'json'})  }
-
-      it "should be unsuccessful (bad_request)" do
-        expect(response.status).to eql(400)
+      it "should be successful (ok)" do
+        expect(response.status).to eql(200)
       end
 
-      it "should include a helpful error message" do
-        expect(parsed_response["pagination"]).to include("when supplying pagination parameters, please use both 'page' and 'per_page'")
-      end
-    end
-
-    context "with invalid pagination params (missing 'page')" do
-      let(:response){  get(:index, params: {per_page: 10, format: 'json'})  }
-
-      it "should be unsuccessful (bad_request)" do
-        expect(response.status).to eql(400)
+      it "should be an array" do
+        expect(parsed_response).to be_kind_of(Array)
       end
 
-      it "should include a helpful error message" do
-        expect(parsed_response["pagination"]).to include("when supplying pagination parameters, please use both 'page' and 'per_page'")
-      end
-    end
-
-    context "with pagination params (page 1)" do
-      let(:per_page){ 5 }
-      let(:response){  get(:index, params: {page: 1, per_page: per_page, format: 'json'})  }
-
-      it "should include at maximum the number of requested resources" do
-        expect(parsed_response.count <= per_page).to eql(true)
-      end
-
-      it "should include the requested page of resources (in descending order of resource creation)" do
-        requested_resources = model_class.first(per_page).map{|resource| resource["id"] } # note, resources sort in descending order by default, so calling .first(i) is really like calling .last(i).reverse
-        resources = parsed_response.map{|resource| resource["id"] }
-        expect(resources).to eql(requested_resources)
-      end
-    end
-
-    context "with pagination params (subsequent pages)" do
-      let(:per_page){ 5 }
-      let(:page){ 2 }
-      let(:response){  get(:index, params: {page: page, per_page: per_page, format: 'json'})  }
-
-      it "should include at maximum the number of requested resources" do
-        expect(parsed_response.count <= per_page).to eql(true)
-      end
-
-      it "should include the requested page of resources (in descending order of resource creation)" do
-        requested_resources = model_class.first(page * per_page).last(per_page).map{|resource| resource["id"] } # note, resources sort in descending order by default, so calling .first(i) is really like calling .last(i).reverse
-        resources = parsed_response.map{|resource| resource["id"] }
-        expect(resources).to eql(requested_resources)
+      it "should include all resources" do
+        expect(parsed_response.count).to eql(model_class.count)
+        expect(parsed_response.any?).to be true
       end
     end
   end
